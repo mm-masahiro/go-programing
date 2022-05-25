0a29fa559eeeeb6f0058162318775c6cc30ac316
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-05-26 07:56:45 +0900

    create make func

---
+++ chitchat/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// ServerMux = HTTPのマルチプレクサ
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	// URLパターンと対応するhandlerをServerMuxの中に登録 → 内部的にhttp.Handler()と同じ処理をしている
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// HandleFuncは第一引数にURLをとり、第二引数にハンドラ関数名をとる
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Hello world")
+}
